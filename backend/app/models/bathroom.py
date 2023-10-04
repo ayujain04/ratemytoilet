9bae3218837e7cefbf5d7798ef975d349af38cf0
sashabacotduke <146894098+sashabacotduke@users.noreply.github.com>
2023-10-03 21:44:57 -0400

    Update bathroom.py restrictions

--- backend/app/models/bathroom.py
+++ backend/app/models/bathroom.py
@@ -1,4 +1,6 @@
 from app import db
+from sqlalchemy import event
+from sqlalchemy.orm import validates
 
 class Bathroom(db.Model):
     bn = db.Column(db.String(256), db.ForeignKey('building.name'), primary_key=True)
@@ -7,3 +9,10 @@ class Bathroom(db.Model):
     
     # Relationship
     building = db.relationship('Building', backref=db.backref('bathrooms', lazy=True))
+
+    @validates('gender')
+    def validate_gender(self, key, value):
+        allowed_genders = ["Male", "Female", "Gender Neutral"]
+        if value not in allowed_genders:
+            raise ValueError(f"Gender must be one of {', '.join(allowed_genders)}")
+        return value
