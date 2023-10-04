286c8fd1a485adb20dae28017713b33b6c03a3ee
Holly Zhuang <mingming.zhuang@duke.edu>
2023-10-03 21:03:02 -0400

    Delete backend/models directory

--- backend/models/building.py
+++
@@ -1,4 +0,0 @@
-from app import db
-
-class Building(db.Model):
-    name = db.Column(db.String(256), primary_key=True)
--- backend/models/user.py
+++
@@ -1,4 +0,0 @@
-from app import db
-
-class User(db.Model):
-    username = db.Column(db.String(256), primary_key=True)
