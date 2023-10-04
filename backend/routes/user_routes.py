51f4d2c50e825db6a0a0dece947d8c9e8e853beb
Holly Zhuang <mingming.zhuang@duke.edu>
2023-10-03 21:03:52 -0400

    Delete backend/routes directory

--- backend/routes/user_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from app import app, db
-from app.models.user import User
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    return jsonify([user.username for user in users])
